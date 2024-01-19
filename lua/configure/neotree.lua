87c3fb46499973b0143da0611b2751b8c98f7edf
aing55 <aingbager55@gmail.com>
2024-01-19 04:23:50 +0000

    edit neotree

--- lua/configure/neotree.lua
+++ lua/configure/neotree.lua
@@ -1,4 +1,6 @@
-local map=vim.api.nvim_set_keymap
-local opts={noremap=true, silent=true}
-vim.g.mapleader=' '
-map('n', '<Space>e', ':Neotree toggle<CR>', opts)
+--local map=vim.api.nvim_set_keymap
+--local opts={noremap=true, silent=true}
+--vim.g.mapleader=' '
+--map('n', '<Space>e', ':Neotree toggle<CR>', opts)
+
+vim.keymap.set('n', '<Space>e', ':Neotree toggle<CR>')
